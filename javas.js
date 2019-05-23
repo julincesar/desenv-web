c519a52e1249b9da28db99f0b82bfefb1f9b7390
Júlio Pacheco <julin.cesar@gmail.com>
2019-05-23 15:27:30 -0300

    Novas alterações

--- javas.js
+++ javas.js
@@ -1,11 +1,12 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
+var tarefas = [];
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
@@ -14,127 +15,139 @@ for (i = 0; i < acc.length; i++) {
 }
 
 function adicionarTarefa() {
-    var projeto = document.getElementById("projeto").value;
-    var descricao = document.getElementById("descricao").value;
-    var executor = document.getElementById("executor").value;
-    var prazo = document.getElementById("prazo").value;
+  var projeto = document.getElementById("projeto").value;
+  var descricao = document.getElementById("descricao").value;
+  var executor = document.getElementById("executor").value;
+  var prazo = document.getElementById("prazo").value;
 
-    if (projeto == undefined || projeto == null || projeto == '') {
-        alert('Preencha o projeto!');
-        return;
-    }
-
-    if (descricao == undefined || descricao == null || descricao == '') {
-        alert('Preencha a descrição!');
-        return;
-    }
-
-    if (prazo == undefined || prazo == null || prazo == '') {
-        alert('Preencha o prazo!');
-        return;
-    }
-
-    var projElem = document.getElementById(projeto);
-
-    if (projElem == null) {
-        //Cria um novo projeto se não tiver encontrado um existente
-        projElem = criarProjeto(projeto);
-    }
-
-    var iden = makeid(6);
-    var lista = criarLista(iden, projeto, descricao, executor, prazo);
-    projElem.appendChild(lista);
-
-    document.getElementById("projeto").value = "";
-    document.getElementById("descricao").value = "";
-    document.getElementById("executor").value = "";
-    document.getElementById("prazo").value = "";
+  if (projeto == undefined || projeto == null || projeto == '') {
+    alert('Preencha o projeto!');
+    return;
   }
 
-  function excluir(noh) {
-    document.getElementById(noh).remove();
+  if (descricao == undefined || descricao == null || descricao == '') {
+    alert('Preencha a descrição!');
+    return;
   }
 
-  function editar(noh) {
-    /* TODO */
-    alert("A implementar.");
+  if (prazo == undefined || prazo == null || prazo == '') {
+    alert('Preencha o prazo!');
+    return;
+  }
+  var iden = makeid(6);
+  tarefa = {"id": iden,
+            "projeto" : projeto,
+            "descricao" : descricao,
+            "executor" : executor,
+            "prazo" : prazo
+          }
+  tarefas.push(tarefa);
+
+
+  var projElem = document.getElementById(projeto);
+
+  if (projElem == null) {
+    //Cria um novo projeto se não tiver encontrado um existente
+    projElem = criarProjeto(projeto);
   }
 
-function criarLista(iden, projeto, descricao, executor, prazo) {
+  var card = criarCard(tarefa);
+  projElem.appendChild(card);
 
-    var h2El = document.createElement("h2");
-    var t = document.createTextNode(descricao);
-    h2El.appendChild(t);
+  limparForm();
+}
 
-    var spa = document.createElement("p");
-    t = document.createTextNode(executor);
-    spa.appendChild(t);
+function limparForm() {
+  document.getElementById("projeto").value = "";
+  document.getElementById("descricao").value = "";
+  document.getElementById("executor").value = "";
+  document.getElementById("prazo").value = "";
+}
 
-    var para = document.createElement("p");
-    para.setAttribute("class", "price");
-    t = document.createTextNode(prazo);
-    para.appendChild(t);
+function excluir(noh) {
+  document.getElementById(noh).remove();
+}
 
-    var but1 = document.createElement("button");
-    t = document.createTextNode("Editar");
-    but1.appendChild(t);
-    but1.setAttribute("onclick", "editar('" + iden + "')");
+function editar(noh) {
+  /* TODO */
+  alert("A implementar.");
+}
 
-    var but2 = document.createElement("button");
-    t = document.createTextNode("Excluir");
-    but2.appendChild(t);
-    but2.setAttribute("onclick", "excluir('" + iden + "')");
+function criarCard(tarefa) {
 
-    var div1 = document.createElement("div");
-    div1.setAttribute("class", "btn-group");
-    div1.appendChild(but1);
-    div1.appendChild(but2);
+  var h2El = document.createElement("h2");
+  var t = document.createTextNode(tarefa.descricao);
+  h2El.appendChild(t);
 
-    var lista = document.createElement("div");
-    lista.setAttribute("id", iden);
-    lista.setAttribute("class", "card");
-    lista.appendChild(h2El);
-    lista.appendChild(spa);
-    lista.appendChild(para);
-    lista.appendChild(div1);
-    
-    return lista;
+  var spa = document.createElement("p");
+  t = document.createTextNode(tarefa.executor);
+  spa.appendChild(t);
+
+  var para = document.createElement("p");
+  para.setAttribute("class", "price");
+  t = document.createTextNode(tarefa.prazo);
+  para.appendChild(t);
+
+  var but1 = document.createElement("button");
+  t = document.createTextNode("Editar");
+  but1.appendChild(t);
+  but1.setAttribute("onclick", "editar('" + tarefa.id + "')");
+
+  var but2 = document.createElement("button");
+  t = document.createTextNode("Excluir");
+  but2.appendChild(t);
+  but2.setAttribute("onclick", "excluir('" + tarefa.id + "')");
+
+  var div1 = document.createElement("div");
+  div1.setAttribute("class", "btn-group");
+  div1.appendChild(but1);
+  div1.appendChild(but2);
+
+  var card = document.createElement("div");
+  card.setAttribute("id", tarefa.id);
+  card.setAttribute("class", "card");
+  card.appendChild(h2El);
+  card.appendChild(spa);
+  card.appendChild(para);
+  card.appendChild(div1);
+
+  return card;
 }
 
 function criarProjeto(projeto) {
 
-    var butEl = document.createElement("button");
-    var t = document.createTextNode(projeto);
-    butEl.appendChild(t);
-    butEl.setAttribute("class", "accordion");
-    
-    butEl.addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-    
-    var divpanel = document.createElement("div");
-    divpanel.setAttribute("class", "panel flex-container");
-    divpanel.setAttribute("id", projeto);
+  var butEl = document.createElement("button");
+  var t = document.createTextNode(projeto);
+  butEl.appendChild(t);
+  butEl.setAttribute("class", "accordion");
 
-    document.getElementById("Projetos").appendChild(butEl);
-    document.getElementById("Projetos").appendChild(divpanel);
-    return divpanel;
- }
+  butEl.addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+
+  var divpanel = document.createElement("div");
+  divpanel.setAttribute("class", "panel flex-container");
+  divpanel.setAttribute("id", projeto);
+
+  document.getElementById("Projetos").appendChild(butEl);
+  document.getElementById("Projetos").appendChild(divpanel);
+  return divpanel;
+}
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 
